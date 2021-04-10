04407e2d38441b4c63f19125a8aa4720ed085817
Ale Sarmiento <alejandragsarmiento@gmail.com>
2021-04-10 12:48:09 +0200

    upgrade hbs, update readme

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,45 +1,48 @@
 export default {
-  server: {
-    port: 8080,
-  },
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt',
-    htmlAttrs: {
-      lang: 'en',
+    server: {
+        port: 8080,
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'nuxt',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            { hid: 'description', name: 'description', content: '' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 }
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -5,11 +5,7 @@
 </template>
 
 <script>
-export default {
-    mounted() {
-        console.log(process.env.NODE_ENV)
-    },
-}
+export default {}
 </script>
 
 <style>
