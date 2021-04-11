40888a07292820eeccee1ad06c6eb58bee23c869
Ale Sarmiento <alejandragsarmiento@gmail.com>
2021-04-11 13:19:55 +0200

    scss file setup  & basic config

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,10 +1,35 @@
+import path from 'path'
+const isProduction = process.env.NODE_ENV === 'production'
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+let envFileName
+if (isProduction) {
+    envFileName = '.env.production.local'
+} else if (isDevelopment) {
+    envFileName = '.env.development.local'
+}
+
+require('dotenv').config({
+    path: path.resolve(process.cwd(), '.env', envFileName),
+})
+
 export default {
     server: {
         port: 8080,
     },
+    /*
+     ** Nuxt rendering mode
+     ** See https://nuxtjs.org/api/configuration-mode
+     */
+    mode: 'universal',
+    /*
+     ** Nuxt target
+     ** See https://nuxtjs.org/api/configuration-target
+     */
+    target: 'server',
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'nuxt',
+        title: process.env.npm_package_name || '',
         htmlAttrs: {
             lang: 'en',
         },
@@ -14,13 +39,24 @@ export default {
                 name: 'viewport',
                 content: 'width=device-width, initial-scale=1',
             },
-            { hid: 'description', name: 'description', content: '' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || '',
+            },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap',
+            },
+        ],
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [],
+    css: ['@/assets/scss/main.scss'],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
@@ -32,6 +68,14 @@ export default {
     buildModules: [
         // https://go.nuxtjs.dev/eslint
         '@nuxtjs/eslint-module',
+        // Doc: https://github.com/nuxt-community/dotenv-module
+        [
+            '@nuxtjs/dotenv',
+            {
+                path: './',
+                filename: envFileName,
+            },
+        ],
     ],
 
     // Modules: https://go.nuxtjs.dev/config-modules
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -8,13 +8,4 @@
 export default {}
 </script>
 
-<style>
-body {
-    padding: 50px;
-    font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif;
-}
-
-a {
-    color: #00b7ff;
-}
-</style>
+<style></style>
